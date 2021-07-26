8b2296d9339aba339c9411c2fc2e0540b62ab702
sejesila <silaseje5@gmail.com>
2021-07-26 06:50:14 +0300

    random number generator

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
-const hex = document.querySelector(".hex")
-const btn = document.querySelector(".gen")
+let number = document.querySelector(".number")
+let btn = document.querySelector(".gen")
 
-const generateColor = () => {
-    let randomColor = Math.random().toString(16).substring(2, 8)
-    document.body.style.backgroundColor = "#" + randomColor;
-    hex.innerHTML = "#" + randomColor;
-    
-};
-btn.addEventListener("click", generateColor)
-
-/* The substring() method returns the part of the string between the start and end indexes, or to the end of the string 
-Syntax
-substring(indexStart, indexEnd)
-substring() extracts characters from indexStart up to but not including indexEnd
-*/
+generateNumber = () =>
+{
+    const rand = Math.floor(Math.random() * 10 +1)
+    number.innerHTML = rand
+}
+btn.addEventListener("click",generateNumber)
+generateNumber()
 
 
 
